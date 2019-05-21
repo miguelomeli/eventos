d4bfc8f136954dc14381dcf25c78d2791ee1dbe4
unknown <firerayken@gmail.com>
2019-05-20 23:09:15 -0500

    se agrega segundo slider

--- src/app/web/home/components/slider/sliders/slider2/slider2.component.ts
+++ src/app/web/home/components/slider/sliders/slider2/slider2.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
+declare var Waypoint: any;
 
 @Component({
   selector: 'app-slider2',
@@ -10,6 +12,14 @@ export class Slider2Component implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    var waypoint = new Waypoint({
+      element: document.getElementById('id_slider_2'),
+      handler: function (direction) {
+        $('.slider2-img-1').addClass('fadeInLeft');
+        $('.slider2-img-1').removeClass('ocultar');
+      },
+      offset: 'bottom-in-view'
+    });
   }
 
 }
